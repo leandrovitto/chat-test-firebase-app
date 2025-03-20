ded24de487fee33faac48d9146d224e073086cb8
Leandro <vitto.leandro@gmail.com>
2025-03-20 10:58:14 +0100

    add Tailwind CSS support and environment configuration to Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
+const env = loadEnv(process.env.NODE_ENV as string, process.cwd(), "VITE_");
+
+// console.log(env);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [tailwindcss(), react()],
+  define: {
+    "process.env": env,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
